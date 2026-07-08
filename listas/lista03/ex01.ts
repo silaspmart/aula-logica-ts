816fda3a44657b4af440f00dac2eaa6b267eb320
Silas Pereira Martins <pet0033648@aluno.firjansenaisesi.com.br>
2026-07-08 19:49:22 -0300

    Adiciona a lista 03

---
+++ listas/lista03/ex01.ts
@@ -0,0 +1,12 @@
+// 01 · Contar até N
+// Leia um número N e escreva todos os números de 1 até N, um do lado do outro.
+
+let N = Number(prompt("Digite um numero:"));
+
+
+let Num = Number(prompt("Até quanto contar?"));
+let contador = "";
+for (let i = 1; i <= n; i++) {
+  contador = contador + i + " ";
+}
+alert(contador);
