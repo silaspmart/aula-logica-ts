3ec845a3c174ae06ae7a9f473e98276ff089681e
Silas Pereira Martins <silaspm_silas@hotmail.com>
2026-07-09 13:19:55 -0300

    Adiciona exercícios da Lista 03

--- listas/lista03/ex01.ts
+++ listas/lista03/ex01.ts
@@ -1,12 +1,9 @@
 // 01 · Contar até N
 // Leia um número N e escreva todos os números de 1 até N, um do lado do outro.
 
-let N = Number(prompt("Digite um numero:"));
-
-
 let Num = Number(prompt("Até quanto contar?"));
 let contador = "";
-for (let i = 1; i <= n; i++) {
+for (let i = 1; i <= Num; i++) {
   contador = contador + i + " ";
 }
 alert(contador);
---
+++ listas/lista03/ex02.ts
@@ -0,0 +1,11 @@
+// 02 · Contagem regressiva
+// Leia N e conte de N até 1 (de trás pra frente). 
+// No fim, solte um 🚀.
+
+let N = Number(prompt("Contagem regressiva:"));
+let cont = "";
+
+for (let i=N; i=1; i--){
+    cont = cont + i + " ";
+}
+alert (cont + "🚀");
